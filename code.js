a34995f5bec4feb8fde0aef3183501a63c86838f
VolleyballofDeath <96141531+VolleyballofDeath@users.noreply.github.com>
2025-05-20 15:53:21 +0000

    added a whole bunch of objects

--- code.js
+++ code.js
@@ -2,12 +2,62 @@
 
 
 function main(){
+    
+    let LocationForest00 = new location([],[],"you awake here with no memory, holding an old rusty sword");
+    let LocationForest01 = new location([new enemy_slime,new enemy_slime],[new item_blackberry(4)],"you come across a secluded grove");
 
+    let Matt = new matt;
 }
+
+
+class location{
+    constructor(enemies,loot,desc){
+        this.enemies = enemies;
+        this.loot = loot;
+        this.desc = desc;
+    }
+}
+
+
 
 class matt{
     constructor(){
-    this.inventory = ["rusty_sword"];
+    this.inventory = [new rusty_sword(1)];
     this.health = 30;
+    }
+}
+
+class item_rusty_sword{
+    constructor(ammount){
+        this.ammount=ammount;
+        this.attack = 3;
+        this.value = 1;
+        this.desc = "an old rusty sword"
+    }
+}
+
+class item_strange_goo{
+    constructor(ammount){
+        this.ammount=ammount;
+        this.value = 0.1;
+        this.desc = "a blob of something left over by a slime. gross"
+    }
+}
+
+class item_blackberry{
+    constructor(ammount){
+        this.ammount = ammount;
+        this.value = 0.5;
+        this.food = 1;
+        this.desc = "a small blackberry. looks edible"
+    }
+}
+
+class enemy_slime{
+    constructor(){
+        this.health = 5 + Number(2*Math.random());
+        this.attack = 2;
+        this.defense = 1;
+        this.drops = [item_strange_goo(5 + Number(2*Math.random()))]
     }
 }
